54a51ab48976e7e0517e6e9d01ce7952fedd0f0e
AlejoCasti <acastiblanco8@gmail.com>
2020-09-01 19:23:18 +0000

    [ADD] project

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' script that runs Flask to response with Hello Holberton '''
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    ''' return value to display '''
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    ''' main function '''
+    app.run(host='0.0.0.0', port=5000)
