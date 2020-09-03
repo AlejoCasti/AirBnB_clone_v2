21ff5974efbedc782e9dd99cf27aae4019f60376
AlejoCasti <acastiblanco8@gmail.com>
2020-09-03 04:52:19 +0000

    [ADD] 9 task

---
+++ web_flask/9-states.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+''' script that runs Flask to response with Hello Holberton '''
+from models import storage
+from models.state import State
+from os import environ
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_down(error):
+    ''' ends every process '''
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_state(id=""):
+    """ displays a HTML page with a list of cities by states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    found = 0
+    state = ""
+    cities = []
+
+    for i in states:
+        if id == i.id:
+            state = i
+            found = 1
+            break
+    if found:
+        states = sorted(state.cities, key=lambda k: k.name)
+        state = state.name
+
+    if id and not found:
+        found = 2
+
+    return render_template('9-states.html',
+                           state=state,
+                           array=states,
+                           found=found)
+
+
+if __name__ == '__main__':
+    ''' main function '''
+    app.run(host='0.0.0.0', port=5000)
